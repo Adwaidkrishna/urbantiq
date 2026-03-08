50b632746b1ecd7f5ead076bad261f2b4477aa51
Adwaidkrishna <adwaidkrishna2@gmail.com>
2026-03-08 12:35:03 +0530

    update routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,27 +1,27 @@
 import express from "express";
-import path from "path";    
+import path from "path";
 
 import {
-register,
-verifyOTP,
-resendOTP,
-login,
-forgotPassword,
-resetPassword,
-verifyResetOTP,
+    register,
+    verifyOTP,
+    resendOTP,
+    login,
+    forgotPassword,
+    resetPassword,
+    verifyResetOTP,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("/forgot-password",(req,res)=>{
+router.get("/forgot-password", (req, res) => {
     res.sendFile(path.resolve("views/user/forgot-password.html"))
 })
 
-router.get("/verify-reset-otp",(req,res)=>{
+router.get("/verify-reset-otp", (req, res) => {
     res.sendFile(path.resolve("views/user/verify-reset-otp.html"))
 })
 
-router.get("/reset-password",(req,res)=>{
+router.get("/reset-password", (req, res) => {
     res.sendFile(path.resolve("views/user/reset-password.html"))
 })
 
@@ -42,14 +42,14 @@ router.get("/home", (req, res) => {
 });
 
 
-router.post("/register",register);
-router.post("/verify-otp",verifyOTP);
+router.post("/register", register);
+router.post("/verify-otp", verifyOTP);
 router.post("/resend-otp", resendOTP);
-router.post("/login",login);
-router.post("/forgot-password",forgotPassword)
-router.post("/verify-reset-otp",verifyResetOTP)
-router.post("/reset-password",resetPassword)
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword)
+router.post("/verify-reset-otp", verifyResetOTP)
+router.post("/reset-password", resetPassword)
 
 
-export default router;  
+export default router;
 
