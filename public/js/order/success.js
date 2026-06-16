ecfd9a63b114b1b5f9e0b762f3d90b1502f0ee59
adwaidkrishna <adwaidkrishna2@gmail.com>
2026-06-17 01:54:24 +0530

    redesigned order success page with premium iOS success checkmark animation

--- public/js/order/success.js
+++ public/js/order/success.js
@@ -14,25 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     dateArr.setDate(dateArr.getDate() + 4);
     const deliveryDateStr = dateArr.toLocaleDateString('en-IN', options);
     
-    if(deliveryMsg) {
+    if (deliveryMsg) {
         deliveryMsg.innerHTML = `Your package is expected to arrive by <span class="fw-700">${deliveryDateStr}</span>.`;
-    }
-
-    // CONFETTI CELEBRATION!
-    if (window.confetti) {
-        const count = 200;
-        const defaults = { origin: { y: 0.7 } };
-
-        function fire(particleRatio, opts) {
-            confetti(Object.assign({}, defaults, opts, {
-                particleCount: Math.floor(count * particleRatio)
-            }));
-        }
-
-        fire(0.25, { spread: 26, startVelocity: 55 });
-        fire(0.2, { spread: 60 });
-        fire(0.35, { spread: 100, decay: 0.91, scalar: 0.8 });
-        fire(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 });
-        fire(0.1, { spread: 120, startVelocity: 45 });
     }
 });
